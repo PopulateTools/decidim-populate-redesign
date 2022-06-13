0a1dd5e6db95110917853aa8abc7d8c5f5d6c17e
Hugoren Martinako <aumpfbahn@gmail.com>
2022-06-13 10:41:18 +0200

    responsive version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,22 @@
 const { inherit, current, transparent, white } = require("tailwindcss/colors")
 
+const withOpacity =
+  (variable) =>
+  ({ opacityValue }) =>
+    opacityValue === undefined
+      ? `rgb(var(${variable}))`
+      : `rgb(var(${variable}) / ${opacityValue})`;
+
 module.exports = {
-  // Define paths that don't include the output of the build to prevent infinite loops
-  content: [
-    "./app/views/**/*.html.erb",
-    "./app/cells/**/*.erb",
-    "./app/helpers/**/*.rb",
-    "./app/packs/**/*.js",
-    "/Users/fernando/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-6e7631e08b24/decidim-*/app/views/**/*.html.erb",
-    "/Users/fernando/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-6e7631e08b24/decidim-*/app/cells/**/*.erb",
-    "/Users/fernando/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-6e7631e08b24/decidim-*/app/helpers/**/*.rb",
-    "/Users/fernando/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-6e7631e08b24/decidim-*/app/packs/**/*.js"
-  ],
+  // This content is generated automatically by decidim:webpacker:install task, it
+  // shouldn't be updated manually.
+  // The array must contain all the decidim modules active in the application
+  content: ['/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-accountability','/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-admin','/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-api','/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-assemblies','/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-blogs','/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-budgets','/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-comments','/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-conferences','/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-consultations','/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-core','/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-debates','/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-dev','/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-elections','/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-forms','/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-generators','/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-initiatives','/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-meetings','/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-pages','/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-participatory_processes','/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-proposals','/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-sortitions','/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-surveys','/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-system','/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-templates','/home/hugo/.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/bundler/gems/decidim-8aca5abfb64b/decidim-verifications','.'].flatMap(directory => [
+    `${directory}/app/views/**/*.html.erb`,
+    `${directory}/app/cells/**/*.erb`,
+    `${directory}/app/helpers/**/*.rb`,
+    `${directory}/app/packs/**/*.js`
+  ]),
   // Comment out the next line to disable purging the tailwind styles
   // safelist: [{ pattern: /.*/ }],
   theme: {
@@ -20,12 +25,12 @@ module.exports = {
       current,
       transparent,
       white,
-      primary: "var(--primary, #FF3333)",
-      secondary: "var(--secondary, #155ABF)",
-      tertiary: "var(--tertiary, #EBC34B)",
-      green: "var(--green, #28A745)",
-      red: "var(--red, #ED1C24)",
-      yellow: "var(--yellow, #FFB703)",
+      primary: withOpacity("--primary-rgb"),
+      secondary: withOpacity("--secondary-rgb"),
+      tertiary: withOpacity("--tertiary-rgb"),
+      green: withOpacity("--green-rgb"),
+      red: withOpacity("--red-rgb"),
+      yellow: withOpacity("--yellow-rgb"),
       black: "#020203",
       gray: {
         DEFAULT: "#C0C6CC",
