ede832ab4f1fb31ba57f1a9dc9e349d89a868b10
Fernando Blat <fernando@blat.es>
2022-06-17 10:50:20 +0200

    Simplify script

--- bin/decidim_auto_update.sh
+++ bin/decidim_auto_update.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# handle kill signs
-trap "echo; exit 1" INT TERM
-
-function log() {
-  local columns=$(tput cols)
-  local text="$1"
-
-  printf '=%.0s' $(seq 1 ${columns})
-  printf "%*s\n" $(((${#text}+$columns)/2)) "$text"
-  printf '=%.0s' $(seq 1 ${columns})
-}
-
-
 LOCAL_DECIDIM_SHA=$( grep revision Gemfile.lock | awk '{print $2}')
 REMOTE_DECIDIM_SHA=$(git ls-remote https://github.com/decidim/decidim.git | grep feature/redesign-staging)
 
@@ -21,9 +8,11 @@ if [[ $LOCAL_DECIDIM_SHA == $REMOTE_DECIDIM_SHA ]]; then
   exit 0
 fi
 
-log "Remove previous Gemfile.lock (testing)" && rm Gemfile.lock && bundle install
-log "Lock Gemfile for Linux too" && bundle lock --add-platform x86_64-linux
-log "Clear packages" && rm -rf package*
-log "Updating decidim webpacker config" && bin/rails decidim:webpacker:install
-log "Installing decidim new migrations" && bin/rails decidim:choose_target_plugins && bin/rails railties:install:migrations
-log "Updating application" && gem install bundler --conservative
+rm -f Gemfile.lock &&
+  bundle install &&
+  bundle lock --add-platform x86_64-linux &&
+  rm -rf package* &&
+  bin/rails decidim:webpacker:install &&
+  bin/rails decidim:choose_target_plugins &&
+  bin/rails railties:install:migrations &&
+  gem install bundler --conservative
