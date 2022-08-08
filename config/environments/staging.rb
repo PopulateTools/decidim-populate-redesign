93be7eb71682761d65780e04b9e08ac3007cc18a
Fernando Blat <fernando@blat.es>
2022-08-08 10:50:15 +0200

    Enable SMTP in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -70,28 +70,21 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  ## config.action_mailer.delivery_method = :smtp
-  ## config.action_mailer.smtp_settings = {
-  ##   :address        => Rails.application.secrets.smtp_address,
-  ##   :port           => Rails.application.secrets.smtp_port,
-  ##   :authentication => Rails.application.secrets.smtp_authentication,
-  ##   :user_name      => Rails.application.secrets.smtp_username,
-  ##   :password       => Rails.application.secrets.smtp_password,
-  ##   :domain         => Rails.application.secrets.smtp_domain,
-  ##   :enable_starttls_auto => Rails.application.secrets.smtp_starttls_auto,
-  ##   :openssl_verify_mode => 'none'
-  ## }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address        => Rails.application.secrets.smtp_address,
+    :port           => Rails.application.secrets.smtp_port,
+    :authentication => Rails.application.secrets.smtp_authentication,
+    :user_name      => Rails.application.secrets.smtp_username,
+    :password       => Rails.application.secrets.smtp_password,
+    :domain         => Rails.application.secrets.smtp_domain,
+    :enable_starttls_auto => Rails.application.secrets.smtp_starttls_auto,
+    :openssl_verify_mode => 'none'
+  }
 
   # Background Processing
   config.active_job.queue_adapter = :sidekiq
 
-  ## Mailing
-  config.action_mailer.delivery_method = :letter_opener_web
-  config.action_mailer.smtp_settings = {
-    address: Rails.application.secrets.smtp_address,
-    port: Rails.application.secrets.smtp_port,
-    domain: Rails.application.secrets.smtp_domain
-  }
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
