d2e2a190eae7caad42a3f80a98a5c7265e1119d6
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2024-02-02 15:37:21 +0100

    Recover Gemfile.lock

--- bin/decidim_auto_update.sh
+++ bin/decidim_auto_update.sh
@@ -7,7 +7,7 @@ function log() {
   echo -e "\n==== $1 ===="
 }
 
-log "Setup bundler and dependencies" && rm -f Gemfile.lock && bundle config --delete frozen && bundle install && bundle lock --add-platform x86_64-linux
+log "Setup bundler and dependencies" && bundle config --delete frozen && bundle install && bundle lock --add-platform x86_64-linux
 log "Updating decidim webpacker config" && bin/rails decidim:webpacker:install
 log "Installing decidim new migrations" && bin/rails decidim:choose_target_plugins && bin/rails railties:install:migrations
 
